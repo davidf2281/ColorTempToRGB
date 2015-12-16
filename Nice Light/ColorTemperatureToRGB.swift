ec43a7662cec449490c8d119e2dbba2aef32514e
David Fearon <github@davidfearon.com>
2015-12-16 21:58:24 +0000

    RGB logging

--- Nice Light/ColorTemperatureToRGB.swift
+++ Nice Light/ColorTemperatureToRGB.swift
@@ -26,7 +26,9 @@ extension UIColor
         green = clamp(percentKelvin <= 66 ? (99.4708025861 * log(percentKelvin) - 161.1195681661) : 288.1221695283 * pow(percentKelvin, -0.0755148492));
         
         blue =  clamp(percentKelvin >= 66 ? 255 : (percentKelvin <= 19 ? 0 : 138.5177312231 * log(percentKelvin - 10) - 305.0447927307));
-        
+
+        print("RGB for color temp of \(kelvin): R\(red) G\(green) B\(blue)")
+
         return UIColor(red: red / 255, green: green / 255, blue: blue / 255, alpha: 1)
     }
     
