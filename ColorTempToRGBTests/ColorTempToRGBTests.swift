8072b94f098f7af0312051332ebc6337c6051cbd
davidfsportlobster <david.fearon@10.me>
2018-06-21 18:45:12 +0100

    Add tests and update UIColor extension to be more conventional

---
+++ ColorTempToRGBTests/ColorTempToRGBTests.swift
@@ -0,0 +1,26 @@
+//
+//  ColorTempToRGBTests.swift
+//  ColorTempToRGBTests
+//
+//  Created by David Fearon on 21/06/2018.
+//  Copyright © 2018 David Fearon. All rights reserved.
+//
+
+import XCTest
+
+class ColorTempToRGBTests: XCTestCase
+{
+    func testColorTemperatureOver6600KelvinProducesMaxRGBValues()
+    {
+        let color6600K = UIColor(temperature:6600)
+        
+        var red:CGFloat = 0, green:CGFloat = 0, blue:CGFloat = 0, alpha:CGFloat = 0
+        
+        color6600K.getRed(&red, green:&green,  blue: &blue, alpha: &alpha)
+        
+        XCTAssert(red == 1)
+        XCTAssert(green == 1)
+        XCTAssert(blue == 1)
+        XCTAssert(alpha == 1)
+    }
+}
